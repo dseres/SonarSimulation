108a448ac17e420ac4263851837cf1f240fd5f26
dseres <dseres001@gmail.com>
2022-03-04 16:03:38 +0000

    add thread routines to sensors

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
-#include <chrono>
-#include <ctime>
+#include <ostream>
+#include <thread>
 using namespace std;
 
 template <class T> struct Packet {
@@ -10,33 +10,58 @@ template <class T> struct Packet {
 
   Packet(T _data) : data(_data) {}
 
-  Packet() = default;
-  Packet(const Packet &other) = default;
-  Packet(Packet &&other) = default;
-  Packet &operator=(const Packet &) = default;
-  Packet &operator=(Packet &&) = default;
-  ~Packet() = default;
+  friend ostream &operator<<(ostream &os, const Packet &p) {
+    auto t_c = chrono::high_resolution_clock::to_time_t(p.timestamp);
+    return os << "Packet{ data = " << p.data
+              << "; timestamp = " << put_time(std::localtime(&t_c), "%F %T %z")
+              << "}";
+  }
 };
 
 class Sensor {
 private:
-    long counter = 0;
-    queue<Packet<long>> packet_queue;
+  bool stop_ind = false;
+  long counter = 0;
+  mutex queue_mutex;
+  queue<Packet<long>> packet_queue;
+
 public:
-  Sensor() = default;
-  Sensor(const Sensor &other) = default;
-  Sensor(Sensor &&other) = default;
-  Sensor &operator=(const Sensor &) = default;
-  Sensor &operator=(Sensor &&) = default;
-  ~Sensor() = default;
-
-    //it is a runnable object
-  void operator()(){
-
+  // it is a runnable object
+  void operator()() {
+    while (!stop_ind || counter<=10) {
+      this_thread::sleep_for(1ms * (1 + rand() % 1000));
+      auto packet = Packet<long>(counter++);
+      cout<<"Sending packet :"<<packet<<endl;
+      const lock_guard<mutex> lock(queue_mutex);
+      packet_queue.push(packet);
+    }
   }
+};
+
+class SonarPU {
+private:
+  Sensor main_sensor;
+  Sensor sensor2;
+  Sensor sensor3;
 };
 
 int main(int argc, char **argv) {
   cout << "Helló világ!\n";
+  auto ts = chrono::high_resolution_clock::now();
+  auto t_c = chrono::high_resolution_clock::to_time_t(ts);
+  cout << put_time(std::localtime(&t_c), "%c") << endl;
+
+  Sensor main_sensor;
+  Sensor sensor2;
+  Sensor sensor3;
+
+  // thread tm(main_sensor);
+  // thread t2(sensor2);
+  // thread t3(&sensor3);
+
+  // tm.join();
+  // t2.join();
+  // t3.join();
+
   return 0;
 }
