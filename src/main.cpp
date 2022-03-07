dcf4b85abae64057ea04a9d36602d927fd002ef3
dseres <dseres001@gmail.com>
2022-03-07 09:43:21 +0000

    sensor thread created

--- src/main.cpp
+++ src/main.cpp
@@ -20,48 +20,80 @@ template <class T> struct Packet {
 
 class Sensor {
 private:
+  string name;
   bool stop_ind = false;
   long counter = 0;
   mutex queue_mutex;
   queue<Packet<long>> packet_queue;
+  int min_wait_msec;
+  int max_wait_msec;
 
 public:
+  Sensor(string name, int min_wait_msec = 1, int max_wait_msec = 1000)
+      : name(name), min_wait_msec(min_wait_msec), max_wait_msec(max_wait_msec) {
+  }
+
+  thread start() { return thread(&Sensor::run, this); }
+
+  void stop() { stop_ind = true; }
+
   // it is a runnable object
-  void operator()() {
-    while (!stop_ind || counter<=10) {
-      this_thread::sleep_for(1ms * (1 + rand() % 1000));
-      auto packet = Packet<long>(counter++);
-      cout<<"Sending packet :"<<packet<<endl;
+  void run() {
+    while (!stop_ind ) {
+      send_packet(Packet<long>(counter++));
+      sleep();
+    }
+  }
+  private:
+    void sleep(){
+      int sleep_msec =
+          min_wait_msec + rand() % (max_wait_msec - min_wait_msec + 1);
+      this_thread::sleep_for(1ms * sleep_msec);
+    }
+
+    void send_packet(Packet<long> &&packet){
+      cout << "Sensor " << name << " sending packet :" << packet << endl;
       const lock_guard<mutex> lock(queue_mutex);
       packet_queue.push(packet);
     }
+};
+
+class Simulator {
+  Sensor main_sensor{"Main", 100, 1000};
+  Sensor sec_sensor1{"Secondary1", 100, 300};
+  Sensor sec_sensor2{"Secondary2", 1000, 3000};
+
+  volatile std::sig_atomic_t signal_status;
+
+public:
+  void signal_handler(int sigsignal_statusnal) {
+    this->signal_status = signal_status;
+    cout<<"Stopping sensors...\n";
+    main_sensor.stop();
+    sec_sensor1.stop();
+    sec_sensor2.stop();
+  }
+
+  void run() {
+
+    thread tm = main_sensor.start();
+    thread ts1 = sec_sensor1.start();
+    thread ts2 = sec_sensor2.start();
+
+    tm.join();
+    ts1.join();
+    ts2.join();
   }
 };
 
-class SonarPU {
-private:
-  Sensor main_sensor;
-  Sensor sensor2;
-  Sensor sensor3;
-};
+Simulator s;
+
+void signal_handler(int signal) { s.signal_handler(signal); }
 
 int main(int argc, char **argv) {
-  cout << "Helló világ!\n";
-  auto ts = chrono::high_resolution_clock::now();
-  auto t_c = chrono::high_resolution_clock::to_time_t(ts);
-  cout << put_time(std::localtime(&t_c), "%c") << endl;
-
-  Sensor main_sensor;
-  Sensor sensor2;
-  Sensor sensor3;
-
-  // thread tm(main_sensor);
-  // thread t2(sensor2);
-  // thread t3(&sensor3);
-
-  // tm.join();
-  // t2.join();
-  // t3.join();
-
+  std::signal(SIGINT, signal_handler);
+  std::signal(SIGABRT, signal_handler);
+  std::signal(SIGTERM, signal_handler);
+  s.run();
   return 0;
 }
